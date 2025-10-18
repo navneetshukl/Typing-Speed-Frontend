bc079163b842158f151ace6b988bcb2b89433135
navneetshukl <navneetshukla824@gmail.com>
2025-10-18 21:53:47 +0530

    created register and signin  code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import TypingTest from './components/TypingTest'
+import Auth from './components/Auth'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <TypingTest/>
+      {/* <TypingTest/> */}
+      <Auth/>
+     
     </>
   )
 }
